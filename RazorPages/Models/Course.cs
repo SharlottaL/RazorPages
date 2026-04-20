a962e30a395fcc160eb6cdccf751933db2a19ac6
SharlottaL <botanichkagigantika@gmail.com>
2026-04-20 16:19:24 +0300

    ContosoDataBase created

--- RazorPages/Models/Course.cs
+++ RazorPages/Models/Course.cs
@@ -7,7 +7,7 @@ namespace RazorPages.Models
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int CourseID { get; set; }
         public string Title { get; set; }
-        public string Credits { get; set; }
+        public int Credits { get; set; }
 
 
         //NP//
--- RazorPages/Program.cs
+++ RazorPages/Program.cs
@@ -1,7 +1,15 @@
+﻿using ContosoUniversity.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RazorPages.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<RazorPagesContosoUniversityContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("RazorPagesContosoUniversityContext") ?? throw new InvalidOperationException("Connection string 'RazorPagesContosoUniversityContext' not found.")));
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 var app = builder.Build();
 
@@ -12,8 +20,20 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+else
+{
+    app.UseDeveloperExceptionPage();
+    app.UseMigrationsEndPoint();
+}
+using(IServiceScope scope = app.Services.CreateScope())
+{
+  IServiceProvider services = scope.ServiceProvider;
+    RazorPagesContosoUniversityContext context = services.GetRequiredService<RazorPagesContosoUniversityContext>();
+    context.Database.EnsureCreated();
+    DbInitializer.Initialize(context);
+}
 
-app.UseHttpsRedirection();
+    app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
