574157e90bd71e09f5f932109aceee7b9aa82688
SharlottaL <botanichkagigantika@gmail.com>
2026-04-20 15:22:45 +0300

    Add project files.

---
+++ RazorPages/Models/Course.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace RazorPages.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public string Credits { get; set; }
+
+
+        //NP//
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
